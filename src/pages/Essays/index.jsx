6dfd575f7993c877fb17735f4918db2ffbbeeed9
joaosantosmedeiros <jopesame@gmail.com>
2023-10-11 10:52:53 -0300

    fix: adjust sidebar

--- src/pages/Essays/index.jsx
+++ src/pages/Essays/index.jsx
@@ -1,27 +1,24 @@
 import {
   Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Icon,
-  Img,
   Input,
-  InputGroup,
-  InputRightElement,
+  Flex,
+  Img,
   Text,
   useColorModeValue,
-  useToast
+  Box,
 } from "@chakra-ui/react";
-import { DotIcon } from "../../components/icons/Icons";
+import { DotIcon, SendIcon } from "../../components/icons/Icons";
 
 export default function Essays() {
   const neutralLight0 = useColorModeValue("neutralLight.0", "neutralLight.0")
   const neutralLight1 = useColorModeValue("neutralLight.1", "neutralLight.1")
+  const neutralLight2 = useColorModeValue("neutralLight.2", "neutralLight.2")
+  const neutralDark0 = useColorModeValue("neutralDark.0", "neutralDark.0")
 
 
   return (
     <Flex>
-      <Flex className="sidebar" bgColor={neutralLight0} height={"100vh"} width={"12.83vw"} flexDir={"column"} alignItems={"center"} flexShrink={"0"}>
+      <Flex className="sidebar" bgColor={neutralLight0} height={"100vh"} width={"240px"} flexDir={"column"} alignItems={"center"} flexShrink={"0"}>
         <Flex align={"center"} justifyContent={"center"} padding={"24px"} ><img src="/src/assets/img/dashboards/logo.svg" alt="logo" /></Flex>
         <Flex padding={"16px 12px"} flexDir={"column"} justifyContent={"space-between"} alignItems={"flex-start"} flex={"1 0 0"} alignSelf={"stretch"}>
           <Flex flexDir={"column"} alignItems={"flex-start"} gap={"16px"} alignSelf={"stretch"}>
@@ -38,15 +35,12 @@ export default function Essays() {
               <path d="M0 1H216" stroke="#F3F4F7" strokeWidth={"1px"} />
             </svg>
             <Flex className="item_menu" padding={"8px 10px"} alignItems={"center"} gap={"8px"} alignSelf={"stretch"}>
-                <Img width={"24px"} borderRadius={"9999px"} src="/src/assets/img/avatars/avatar5.png" alt="profile image"></Img>
-                <Text width={"100%"} display={"flex"} justifyContent={"flex-start"} textStyle={"Caption"}>Hector Lima</Text>
-                <DotIcon/>
+              <Img width={"24px"} borderRadius={"9999px"} src="/src/assets/img/avatars/avatar5.png" alt="profile image"></Img>
+              <Text width={"100%"} display={"flex"} justifyContent={"flex-start"} textStyle={"Caption"}>Hector Lima</Text>
+              <DotIcon color={neutralDark0} />
             </Flex>
           </Flex>
         </Flex>
-      </Flex>
-      <Flex className="content" bgColor={neutralLight1} width={"87.17vw"}>
-        
       </Flex>
     </Flex>
   )
