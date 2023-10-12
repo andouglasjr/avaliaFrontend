75a4f904a9b6f5c70b1681fd9a0a68f981d97d42
andouglasjr <andouglas.silva@escolar.ifrn.edu.br>
2023-10-12 19:30:43 -0300

    adder loading animation

--- src/pages/Loading/index.jsx
+++ src/pages/Loading/index.jsx
@@ -6,12 +6,12 @@ import {
 
 import { GenerationEssayIcon } from "../../components/icons/Icons"
 
-function EssayGenerationLoading(){
+function EssayGenerationLoading() {
 
-  const neutralLight1 = useColorModeValue("neutralLight.1","neutralLight.1")
-  const neutralDark4 = useColorModeValue("neutralDark.4","neutralDark.4")
+  const neutralLight1 = useColorModeValue("neutralLight.1", "neutralLight.1")
+  const neutralDark4 = useColorModeValue("neutralDark.4", "neutralDark.4")
 
-  return(
+  return (
     <Flex
       width="100vw"
       height="100vh"
@@ -28,13 +28,11 @@ function EssayGenerationLoading(){
         <Flex
           marginBottom="22px"
         >
-          <GenerationEssayIcon
-            h="200px"
-            w="260px"
-          />
+          <iframe src="https://lottie.host/?file=216aed1b-5cc8-4e90-afc3-f0a1a4628e8d/PnDvH3oTgX.json" alt="Mulher escrevendo um texto"></iframe>
+          <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
         </Flex>
         <Flex>
-          <Text 
+          <Text
             color={neutralDark4}
             fontSize="24px"
             fontWeight="600"
